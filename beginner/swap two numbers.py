5443f94092538f7401718ed7205eb6873a3d6a59
MEKALADEVI <42136989+MEKALADEVI@users.noreply.github.com>
2018-10-03 17:25:24 +0530

    Create swap two numbers.py

---
+++ beginner/swap two numbers.py
@@ -0,0 +1,9 @@
+a1=input("m=")
+a2=input("n=")
+print("a1 before swapping: ",a1)
+print("a2 before swapping: ",a2)
+t=a1
+a1=a2
+a2=t
+print("a1 after swapping: ",a1)
+print("a2 after swapping: ",a2)
