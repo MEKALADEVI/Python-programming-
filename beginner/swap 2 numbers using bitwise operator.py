253a9dc5023bece7867b1f37a2dabc496c6ac515
MEKALADEVI <42136989+MEKALADEVI@users.noreply.github.com>
2018-10-04 17:36:01 +0530

    Create swap 2 numbers using bitwise operator.py

---
+++ beginner/swap 2 numbers using bitwise operator.py
@@ -0,0 +1,9 @@
+a1=int(input("m="))
+a2=int(input("n="))
+print("a1 before swapping: ",a1)
+print("a2 before swapping: ",a2)
+a1=a1^a2
+a2=a1^a2
+a1=a1^a2
+print("a1 after swapping: ",a1)
+print("a2 after swapping: ",a2)
